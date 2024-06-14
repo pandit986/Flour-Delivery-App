e96725bb69d0767072aec93955cdfac923d64fbe
pandit986 <abhishekpandit986@gmail.com>
2024-06-14 10:28:40 +0530

    file chnages

--- navigator/AppNavigation.js
+++ navigator/AppNavigation.js
@@ -3,7 +3,6 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
-import DetailsScreen from '../screens/DetailsScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,7 +11,6 @@ function AppNavigation() {
         <NavigationContainer>
             <Stack.Navigator initialRouteName="Home">
                 <Stack.Screen name="Home" component={HomeScreen} />
-                <Stack.Screen name="Details" component={DetailsScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
